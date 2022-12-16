5c57e59dcb0076244e71df8675c00853e17345bd
David-Jesse <djesse351@gamil.com>
2022-12-16 12:59:24 -0800

    updated the code

---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * add - function that returns sum of two integers
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: integer value of (a + b)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * sub - function that returns subtraction of two integers
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: integer value of (a - b)
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function that returns multiplication of two integers
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: integer value of (a * b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function that returns division of two integers
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: integer value of (a / b)
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function that returns modulus of two integers
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: integer value of (a % b)
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
